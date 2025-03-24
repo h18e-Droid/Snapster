74699b36ee0916347414674941f62a8fe4aa4427
pendlyshakilly <pendlyshakilly@gmail.com>
2025-03-24 12:47:19 +0100

    refactor: refactor pagination styles sheet

--- src/shared/ui/pagination/Pagination.tsx
+++ src/shared/ui/pagination/Pagination.tsx
@@ -2,27 +2,32 @@ import React, { useState } from "react"
 import PageNavigator from "./components/pageNavigator/PageNavigator"
 import PageSizeSelector from "./components/selector/PageSizeSelector"
 import styles from "./Pagination.module.scss"
+import clsx from "clsx"
 
 type Props = {
   totalSize: number
+  onChange: (currentPage: number, portionSize: number) => void
+  className?: string
 }
 
-const Pagination = ({ totalSize }: Props) => {
+export const Pagination = ({ totalSize, onChange, className }: Props) => {
   const [currentPage, setCurrentPage] = useState(1)
   const [portionSize, setPortionSize] = useState<number>(10)
 
   const setPortionSizeHandler = (value: number) => {
     setPortionSize(value)
+    onChange(currentPage, portionSize)
   }
 
   const setCurrentPageHandler = (page: number) => {
     setCurrentPage(page)
+    onChange(currentPage, portionSize)
   }
 
   const totalPages = Math.ceil(totalSize / portionSize)
 
   return (
-    <div className={styles.paginationContainer}>
+    <div className={clsx(styles.paginationContainer, className)}>
       <PageNavigator currentPage={currentPage} setCurrentPage={setCurrentPageHandler} totalPages={totalPages} />
       <span className={styles.text}>Show</span>
       <PageSizeSelector setPortionSize={setPortionSizeHandler} portionSize={portionSize} />
@@ -30,5 +35,3 @@ const Pagination = ({ totalSize }: Props) => {
     </div>
   )
 }
-
-export default Pagination
