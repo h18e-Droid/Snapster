c71a3a5317bd8457dcc7f0deaac1e654ea5bbba9
h18e-Droid <ivazostazh@gmail.com>
2025-03-24 09:49:41 +0300

    refactor: project migrate to Feature Sliced Design

--- app/__tests__/fixErrorTest.test.ts
+++ app/__tests__/fixErrorTest.test.ts
@@ -2,6 +2,6 @@ import fixErrorTest from "@/app/fixErrorTest"
 
 describe("Fix error", () => {
   it("should be 2", () => {
-    expect(fixErrorTest(1, 1)).toBe(2)
+    expect(fixErrorTest(5, 3)).toBe(2)
   })
 })
