e8e77c297ae2a9b1a7574ba7230f89d5b8ff6b0b
pendlyshakilly <pendlyshakilly@gmail.com>
2025-03-24 09:25:29 +0100

    refactor: move textArea in the right place

--- src/shared/ui/TextArea/TextArea.tsx
+++ src/shared/ui/TextArea/TextArea.tsx
@@ -1,34 +1,24 @@
 import React, { ComponentPropsWithoutRef, CSSProperties } from "react"
-import s from "@/components/ui/TextArea/TextArea.module.css"
+import s from "./TextArea.module.css"
 
 type TextAreaProps = ComponentPropsWithoutRef<"textarea"> & {
-  width: string | number,
-  height: string | number,
+  width: string | number
+  height: string | number
   error?: boolean | string
 }
 
-
 export const TextArea = ({ width, height, error, ...props }: TextAreaProps) => {
-
-
   const buttonStyle: CSSProperties = {
     width: width || 284,
-    height: height || 84
+    height: height || 84,
   }
 
   return (
     <div className={s.container}>
-    <textarea
-      className={props.className}
-      style={{ ...buttonStyle }}
-      value={props.value}
-    >{props.defaultValue}</textarea>
-      {error &&
-        <label className={s.labelError}>Error text</label>
-      }
-
+      <textarea className={props.className} style={{ ...buttonStyle }} value={props.value}>
+        {props.defaultValue}
+      </textarea>
+      {error && <label className={s.labelError}>Error text</label>}
     </div>
-
   )
 }
-
