6e4b8706969723794cb1aeffd9bf689bd629cffe
pendlyshakilly <pendlyshakilly@gmail.com>
2025-03-26 19:54:26 +0100

    SCRUM-33 header: mini-fix-Header

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -4,7 +4,7 @@ import styles from "./Navbar.module.scss"
 export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <div style={{ width: "100%", height: "100%" }}>Navbar</div>
+      <div>Navbar</div>
     </nav>
   )
 }
