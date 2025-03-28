8fb349a3f8acb634422e765f8e489d3de7ea1754
Niko-krasulin <ninja@MacBook-Air-Nikita.local>
2025-03-28 08:49:21 +0200

    fix: type name and, s change styles

--- src/shared/ui/tabs/Tabs.tsx
+++ src/shared/ui/tabs/Tabs.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Tabs } from "radix-ui"
-import s from "./Tabs.module.css"
+import styles from "./Tabs.module.css"
 
 
 export type Tab = {
@@ -9,7 +9,7 @@ export type Tab = {
   content: React.ReactNode | string;
 }
 
-export type TabPropsType = {
+export type TabProps = {
   triggerClassName?: string,
   contentClassName?: string
   disabled?: boolean
@@ -21,11 +21,11 @@ export const Tab = ({
                       contentClassName,
                       disabled,
                       tabs
-                    }: TabPropsType) => {
+                    }: TabProps) => {
 
   return (
-    <Tabs.Root className={s.tabRoot}>
-      <Tabs.List className={s.tabList}>
+    <Tabs.Root className={styles.tabRoot}>
+      <Tabs.List className={styles.tabList}>
         {tabs.map((tab: Tab) => {
           return (
             <Tabs.Trigger className={triggerClassName} value={tab.id} key={tab.id} disabled={disabled}>
