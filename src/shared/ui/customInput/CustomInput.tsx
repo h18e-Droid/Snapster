129d88a94c09481793bfb45e02bf3c72e5da509b
NovikovRomanV <rimm19845@gmail.com>
2025-03-30 19:11:38 +0300

    fix: replaced colors in scss with colors from _colors.scss, used the clsx library, fixed the work of activ in the CustomInput component.

--- src/shared/ui/customInput/CustomInput.tsx
+++ src/shared/ui/customInput/CustomInput.tsx
@@ -5,6 +5,7 @@ import React, { ChangeEvent, useEffect, useState } from "react"
 import { EyeIcon } from "@/shared/assets/icons/components/EyeIcon"
 import { EyeOffIcon } from "@/shared/assets/icons/components/EyeOffIcon"
 import { SearchIcon } from "@/shared/assets/icons/components/SearchIcon"
+import clsx from "clsx"
 
 type Props = {
   type: string
@@ -18,15 +19,25 @@ type Props = {
 }
 
 export const CustomInput = (props: Props) => {
-  const { type, placeholder, value, onChange, disabled, label, width, errorText } = props
 
-  const colorDisabled = "#4C4C4C"
-  const colorSecondary = "#8D9094"
+  const { type, placeholder, value, onChange, disabled, label, width, errorText } = props
 
   const [isPasswordVisible, setIsPasswordVisible] = useState(false)
   const [emailError, setEmailError] = useState<string | null>(null)
   const [inputValue, setInputValue] = useState(value)
   const [isSearchVisible, setIsSearchVisible] = useState(false)
+  const [active, setActive] = useState(false)
+
+  const inputClassName = clsx(
+    styles.input,
+    disabled ? styles.disabled : "",
+    emailError && styles.errorInput,
+    active && styles.activeInput,
+  )
+  const searchIconClassName = clsx(styles.icon, disabled && styles.iconDisabled)
+  const inputContainerClassName = clsx(styles.inputContainer, disabled ? styles.disabled : "")
+  const placeholderTextClassName = clsx(styles.placeholderText, disabled ? styles.disabled : "")
+  const eyeIconPasswordClassName = clsx(styles.eyeIcon, disabled ? styles.disabled : "")
 
   useEffect(() => {
     setInputValue(value)
@@ -48,6 +59,9 @@ export const CustomInput = (props: Props) => {
   }
 
   useEffect(() => {
+    if (inputValue.length > 0) {
+      setActive(true)
+    }
     if (!disabled && inputValue.length > 0 && type === "email") {
       const timer = setTimeout(() => {
         if (!isValidEmail(inputValue)) {
@@ -56,41 +70,44 @@ export const CustomInput = (props: Props) => {
           setEmailError(null)
         }
       }, 3000)
-
       return () => clearTimeout(timer)
     } else {
       setEmailError(null)
     }
   }, [inputValue, type, disabled])
 
+
+
   return (
     <div className={styles.inputBox}>
       {label && <label className={styles.label}>{label}</label>}
-      <div className={`${styles.inputContainer} ${disabled ? styles.disabled : ""}`} style={{ width }}>
+      <div className={inputContainerClassName} style={{ width }}>
         {type === "search" && !isSearchVisible && !inputValue.length && (
           <div className={styles.placeholderContainer}>
-            <SearchIcon color={disabled ? colorDisabled : colorSecondary} className={styles.icon} />
-            <span className={`${styles.placeholderText} ${disabled ? styles.disabled : ""}`}>{placeholder}</span>
+            <SearchIcon className={searchIconClassName} />
+            <span className={placeholderTextClassName}>{placeholder}</span>
           </div>
         )}
         <input
-          className={`${styles.input} ${disabled ? styles.disabled : ""} ${emailError && styles.errorInput}`}
+          className={inputClassName}
           type={type === "password" && !isPasswordVisible ? "password" : "text"}
           placeholder={type === "search" ? "" : placeholder}
           value={inputValue}
           onChange={onChangeHandler}
           disabled={disabled}
           onMouseDown={() => {
+            setActive(true)
             setIsSearchVisible(true)
           }}
           onBlur={() => {
+            setActive(false)
             if (!inputValue) {
               setIsSearchVisible(false)
             }
           }}
         />
         {type === "password" && (
-          <span className={`${styles.eyeIcon} ${disabled ? styles.disabled : ""}`} onClick={togglePasswordVisibility}>
+          <span className={eyeIconPasswordClassName} onClick={togglePasswordVisibility}>
             {isPasswordVisible ? <EyeIcon /> : <EyeOffIcon />}
           </span>
         )}
